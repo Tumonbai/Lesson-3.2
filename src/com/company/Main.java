dc353851d4842cb597d5b3f30e2928fb5e1b176b
Tumonbai <ztima90@gmail.com>
2019-12-03 21:31:05 +0600

    lesson 3.2

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = -2; i < 5; i++) // i = i + 1
+            System.out.println("Hello");
+
+        int i = 9;
+        while (i>=1){
+            System.out.println("9x" + i+ "= + (9*i)");
+            i-=2;
+        }
+
+        String str = "";
+        while ( (str.length()<10){
+        }
+    }
+    }
